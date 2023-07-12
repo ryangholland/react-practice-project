57436d55fd1c1e8c3fb8025c8ff827752d465b22
ryangholland <rghcode@gmail.com>
2023-07-11 20:43:55 -0400

    Allow user to type in textbox and add tasks

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,37 @@ class App extends Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      inputValue: "",
+      tasks: []
+    };
+
+    this.updateInput = this.updateInput.bind(this);
+    this.updateTasks = this.updateTasks.bind(this);
+  }
+
+  updateInput(text) {
+    this.setState({
+      inputValue: text,
+    });
+  }
+
+  updateTasks() {
+    this.state.tasks.push(this.state.inputValue);
+    this.updateInput("");
+    console.log(this.state.tasks);
   }
 
   render() {
-    return <h1>Hello.</h1>;
+    return (
+      <>
+        <input
+          value={this.state.inputValue}
+          onChange={(e) => this.updateInput(e.target.value)}
+        ></input>
+        <button onClick={this.updateTasks}>Submit</button>
+      </>
+    );
   }
 }
 
