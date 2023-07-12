f6a8a31930e475b906f3808230213891b00d5022
ryangholland <rghcode@gmail.com>
2023-07-11 20:28:25 -0400

    Set up react and initial files

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return <h1>Hello.</h1>;
+  }
+}
+
+export default App;
